b08284663c4c7905c820cd9a06e229696b6b086a
«Vlad» <>
2023-01-10 18:44:03 +0300

    Initial commit

---
+++ src/Task_2085.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Task_2085 {
+    public static void main(String[] args) {
+
+    }
+
+    public int countWords(String[] words1, String[] words2) {
+        Map<String, Integer> cnt1 = count(words1);
+        Map<String, Integer> cnt2 = count(words2);
+        int ans = 0;
+        for (String w : words1) {
+            if (cnt1.getOrDefault(w, 0) == 1 && cnt2.getOrDefault(w, 0) == 1) {
+                ++ans;
+            }
+        }
+        return ans;
+    }
+
+    private Map<String, Integer> count(String[] words) {
+        Map<String, Integer> cnt = new HashMap<>();
+        for (String w : words) {
+            cnt.put(w, cnt.getOrDefault(w, 0) + 1);
+        }
+        return cnt;
+    }
+}
